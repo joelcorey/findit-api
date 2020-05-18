f1893e6d28bbad3e87c69fe4ee707824e6dd1186
Joel Corey <joelcorey@fastmail.com>
2020-05-18 07:50:22 -0700

    update comments

--- city-build-urls/city-build-urls-service.js
+++ city-build-urls/city-build-urls-service.js
@@ -25,55 +25,14 @@ const cityBuildUrlsService = {
         )
     },
 
+    // DONE: Find all cities in a state
 
-    /*
-    Working raw SQL statement for getting count of cities in each state:
-        SELECT territory_name, city_country, COUNT(*) AS total 
-        FROM city_urls 
-        GROUP BY territory_name, city_country 
-        ORDER BY territory_name;
+    // TO FRONT END: Find all cities in a region (Pacific Northwest, West Coast, Midwest, etc)
 
-    */
-    getCityUrlsCounts() {
-        db.city_urls.findAll(
-            {
-                attributes: 
-                [
-                    'territory_name', 
-                    'city_country',
-                    [sequelize.fn('COUNT', sequelize.col('*'))]
-                ],
-                group:
-                [
-                    'territory_name',
-                    'city_country'
-                ],
-                order:
-                [
-                    'territory_name'
-                ]
-            }
-        )
-    }
+    // TO FRONT END: Find specific city, might return multiple if same name of city across multiple states
 
-   
-
-    // Find all cities in a state
-
-    // Find all cities in a region (Pacific Northwest, West Coast, Midwest, etc)
-
-    // Find specific city, might return multiple if same name of city across multiple states
-
-    // Find specific city in specific state
+    // TO FRONT END: Find specific city in specific state
 
 }
 
 module.exports = cityBuildUrlsService;
-
-/*
-get count of cities where territory_name unique, this will inform front end of how many cities are left 
-and other useful data
-
-WORKING:
-SELECT territory_name, city_country, COUNT(*) AS total FROM city_urls GROUP BY territory_name, city_country ORDER BY territory_name;
-*/
