a7ad840dbf5be17b858c777a66bf0816f98f57bf
Joel Corey <joelcorey@fastmail.com>
2020-05-21 08:28:24 -0700

    change endpoints

--- city-scrape-urls/city-scrape-urls-router.js
+++ city-scrape-urls/city-scrape-urls-router.js
@@ -7,31 +7,21 @@ const cityScrapeUrlsService = require('./city-scrape-urls-service');
 const cityScrapeUrlsRouter = express.Router();
 
 cityScrapeUrlsRouter
-	// deprecated first version
-	// .route('/')
-	// .get(async (req, res, next) => {
-	
-	// 	try {
-    //         const cities = await cityScrapeUrlsService.getAllCities();
-    //         const data = await cityScrapeUrlsScraper(cities);
-	// 		res.send(data);
-
-	// 	} catch (error) {
-	// 		return next(error);
-	// 	}
-
 	.route('/')
 	.post(async (req, res, next) => {
 		var data = req.body;
 		vardump(data)
 		try {
 			const cities = await cityScrapeUrlsService.getCitiesInState(data.state);
-			const jobData = await cityScrapeUrlsScraper(cities);
-
-			// Move just cities to a different endpoint?
-			//res.send(cities);
 			
-			res.send(jobData)
+			// This endpoint is returning a nested array, making data itteration difficult for frontend
+			// Move just cities to a different endpoint?
+			res.send(cities);
+
+			// Specific city job data is to be fetched/called on a per job basis
+			// new endpoint: "city"
+			//const jobData = await cityScrapeUrlsScraper(cities);
+			//res.send(jobData)
 		} catch (error) {
 			return next(error);
 		}
